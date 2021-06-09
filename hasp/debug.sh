7b37e6abadbf267155dcc2d7f64b1f50efb3743d
tangfei <tangfeizz@outlook.com>
2021-06-09 17:38:21 +0800

    修复sshpass第一次执行没有反应

--- hasp/debug.sh
+++ hasp/debug.sh
@@ -4,8 +4,10 @@ echo '开始编译项目和打包'
 mvn clean package -DskipTests
 
 #部署的机器IP
-host=172.26.1.22
+user=admin
+host=172.26.10.60
+pass=admin
 
 #sshpass 需要单独安装
 echo '开始上传jar到目标服务器上' $host
-sshpass -p 123456 scp ./target/hasp-0.0.1-SNAPSHOT.jar root@$host:/root
+sshpass -p $pass scp -q -o StrictHostKeyChecking=no ./target/hasp-0.0.1-SNAPSHOT.jar $user@$host:/home/admin
